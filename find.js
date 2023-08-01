6ad36e93a87031dd5a3ffd347badbb2751188bc1
lMaximix <maximix.07@mail.ru>
2023-08-01 19:15:37 +0300

    релиз2

--- find.js
+++ find.js
@@ -1,13 +1,19 @@
-function firstUnder(numbers, limit){
+function firstUnder(numbers, limit) {
   for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] <= limit)
-    return numbers[i]
+    if (numbers[i] <= limit) return numbers[i];
   }
 }
 
-function startsWith(strings, letter){
+function firstUnder(numbers, limit) {
+  return numbers.find((el) => el <= limit);
+}
+
+function startsWith(strings, letter) {
   for (let i = 0; i < strings.length; i++) {
-    if (strings[i][0] === letter)
-    return strings[i]
+    if (strings[i][0] === letter) return strings[i];
   }
+}
+
+function startsWith(strings, letter) {
+  return strings.find((el) => el === letter);
 }
